b373782fd76dfc5483f4bcdfc47ae4b04465a1e4
natenettey <niibobby4@gmail.com>
2022-08-23 15:39:45 +0000

    completed navbar module

--- src/UI/Navbar.js
+++ src/UI/Navbar.js
@@ -1,8 +1,21 @@
+import logo from '../assets/mortgagey.png'
+import classes from '../UI/Navbar.module.css'
 const Navbar = ()=>{
     return(
-        <nav>
-            
-        </nav>
+        <div className={classes.navbar}>
+            <div ><img src={logo} alt="the logo"/></div>
+            <div>
+                <ul>
+                    <li>
+                    <a href="">Calculate Mortgage</a>
+                    </li>
+                    <li>
+                    <a href="">How this works</a>
+                    </li>
+                </ul>
+                
+            </div>
+        </div>
     )
 }
 
