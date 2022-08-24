271b751f803b7bbc2af518bbb3793911fa631277
natenettey <niibobby4@gmail.com>
2022-08-24 12:12:49 +0000

    added the header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './UI/Navbar';
+import Header from './UI/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
     </div>
   );
 }
