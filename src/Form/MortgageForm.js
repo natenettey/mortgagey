34e44a8558a7af429438435f6d90c2a5081ec27f
natenettey <niibobby4@gmail.com>
2022-09-02 12:43:05 +0000

    set up the statemanagement for the inputs and forms

--- src/Form/MortgageForm.js
+++ src/Form/MortgageForm.js
@@ -1,42 +1,99 @@
 import classes from "../UI/GeneralStyling.module.css"
 import styleclass from "../Form/Form.module.css"
+import { useState } from "react"
 
 
 const MortgageForm = ()=>{
+    //setup the statemanagement for all the inputs
+    const [journeyStatus, setJourneyStatus]= useState()
+    const [savings, setSavings]=useState()
+    const [property, setProperty]=useState()
+    const [propertyType, setPropertyType]=useState()
+    const [salary, setSalary]=useState()
+    const [commitments, setCommitments]=useState()
+    const [loans, setLoans]=useState()
+
+    //form submit handler
+    const submitHandler=()=>{
+
+    }
+
+    //set up Individual input handlers
+
+    const journeyChangeHandler = (e)=>{
+        setJourneyStatus(e.target.value)
+        console.log(journeyStatus)
+    }
+
+    const savingsHandler = (e)=>{
+        setSavings(e.target.value)
+        
+    }
+
+    const propertyHandler = (e)=>{
+        setProperty(e.target.value)
+        
+    }
+
+    const propertyTypeHandler = (e)=>{
+        setPropertyType(e.target.value)
+        
+    }
+
+    const salaryHandler = (e)=>{
+        setSalary(e.target.value)
+        
+    }
+
+    const commitmentsHandler = (e)=>{
+        setCommitments(e.target.value)
+        
+    }
+
+    const loansHandler = (e)=>{
+        setLoans(e.target.value)
+        
+    }
 
     return (
         <div className={classes.spacing}>
             <div className={styleclass.formcontainer}>
-                <form >
+                <form onSubmit={submitHandler} >
                 <div className={styleclass.form_dispplay}>
                 <div className={styleclass.formcontainer_one}>
-                <label>Where are you in your home ownership journey ?</label>
-                    <select>
+
+                <label htmlFor="journey-status">Where are you in your home ownership journey ?</label>
+                    <select id="journey-status" onChange={journeyChangeHandler}>
                     <option value="books">Books</option>
                     <option value="html">HTML</option>
                     <option value="css">CSS</option>
                     </select>
 
-                    <label>How much have you set aside for home purchase ?</label>
-                    <input></input>
+                    <label htmlFor="savings">How much have you set aside for home purchase ?</label>
+                    <input id="savings" type="number" placeholder="In $" onChange={savingsHandler}></input>
 
-                    <label>Do you own any other properties ?</label>
-                    <input></input>
+                    <label htmlFor="property-number">How many other properties do you own ?</label>
+                    <input id="property-number" type="number" onChange={propertyHandler}></input>
+
+                    <label htmlFor="property-type">What property type are you looking for ?</label>
+                    <select id="property-type" onChange={propertyTypeHandler}>
+                    <option value="books">Books</option>
+                    <option value="html">HTML</option>
+                    <option value="css">CSS</option>
+                    </select>
                 </div>
                 
                 <div className={styleclass.formcontainer_two}>
-                <label>How much do you earn ?</label>
-                    <input></input>
+                    <label htmlFor="salary">How much do you earn ?</label>
+                    <input id="salary" type="number" placeholder="In $" onChange={salaryHandler}></input>
 
-                    <label>Do you have monthly commitments ?</label>
-                    <input></input>
+                    <label htmlFor="commitments">Total amount of monthly commitments </label>
+                    <input id="commitments" type="number" placeholder="In $" onChange={commitmentsHandler}></input>
 
-                    <label>What property type are you lookinhg for ?</label>
-                    <select>
-                    <option value="books">Books</option>
-                    <option value="html">HTML</option>
-                    <option value="css">CSS</option>
-                    </select>
+                    <label htmlFor="loan-field">How many existing home loans do you have ?</label>
+                    <input id ="loan-field" type="number" onChange={loansHandler}></input>
+
+                    
                 </div>
                 </div>
                 
