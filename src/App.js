225ed9c3445d486ba23cf040a8715d1c23efe662
natenettey <niibobby4@gmail.com>
2022-09-05 13:00:56 +0000

    created the Mortgage class to handle all calculations

--- src/App.js
+++ src/App.js
@@ -4,13 +4,70 @@ import BodyText from './UI/BodyText';
 import MortgageForm from './Form/MortgageForm';
 import './App.css';
 
-function App() {
+//create class or constructor function here
+class Mortgage{
+  constructor (saved, properties,earnings, deductions, homeLoans){
+    this.saved =saved;
+    this.properties = properties;
+    this.earnings = earnings;
+    this.deductions = deductions;
+    this.homeLoans = homeLoans;
+    this.percentage = 0
+  }
+  loanToValue(){
+    
+    
+    if (this.homeLoans == 0 ){
+      this.percentage = 0.75
+      return this.percentage
+  }else if(this.homeLoans==1){
+     this.percentage = 0.45
+     return this.percentage
+  }else if (this.homeLoans >=2){
+       this.percentage = 0.35
+       return this.percentage     
+  }
+  }
+
+  propertyCost(){
+    const newPercentage = this.loanToValue()
+    const property_value = Math.round(this.saved/(1-newPercentage))
+    return property_value
+  }
+
+  loanAmount(){
+    const percentage = this.loanToValue()
+    const property_value = this.propertyCost()
+    const loan_due = percentage*property_value
+    return loan_due
+  }
+
+}
+
+
+
+function App(props) {
+
+const inputsHandler=(inputs)=>{
+  const {status, saved, properties,type,earnings, deductions,homeLoans} = inputs
+  console.log("new",status, saved, properties,type,earnings, deductions,homeLoans)
+  const newMortgage = new Mortgage(saved, properties,earnings, deductions, homeLoans)
+  let newLoanToValue = newMortgage.loanToValue()
+  let newPropertyCost = newMortgage.propertyCost()
+  let newLoanAmount = newMortgage.loanAmount()
+  console.log("newloan",typeof(newLoanToValue),newLoanToValue,newPropertyCost,newLoanAmount)
+  // let newPropertyCost = newMortgage.propertyCost()
+  // let newLoanAmount = newMortgage.loanAmount()
+
+  // console.log(newLoanAmount,newLoanToValue,newPropertyCost)
+
+}
   return (
     <div className="App">
       <Navbar/>
       <Header/>
       <BodyText/>
-      <MortgageForm/>
+      <MortgageForm onSubmitInput = {inputsHandler}/>
     </div>
   );
 }
