cd9a461e91c5f1d2a374fd43e60c9b491e6c8229
natenettey <niibobby4@gmail.com>
2022-10-03 15:39:49 +0000

    displayed the prop values in the paragraph element

--- src/ResultModal/Modal.js
+++ src/ResultModal/Modal.js
@@ -2,17 +2,52 @@ import Card from "../UI/Card"
 import ReactDOM  from "react-dom"
 import classes from '../ResultModal/Modal.module.css'
 
-const Modal = ()=>{
+const Modal = (props)=>{
     return(
         <>
             {ReactDOM.createPortal(
                 <div className={classes.modal_backdrop}>
                     <Card>
-                        <div>
-                            <h2>Your home could cost up to $5000</h2>
-                            <div>
+                    <h2>Affordability Breakdown</h2>
+                    <h3>Your home could cost up to  ${props.breakDownValues.newPropertyCost.toLocaleString()}</h3>
+                        <div className={classes.modal_breakdown}>
+                            <section className={classes.modal_breakdown__section}>
+                                <div className = {classes.modal_breakdown__section__div}>
+                                <p>Loan Amount :</p>
+                                <p>$ {props.breakDownValues.newLoanAmount}</p>
+                                </div>
+
+                                <div className = {classes.modal_breakdown__section__div}>
+                                <p>Loan To Value :</p>
+                                <p>{props.breakDownValues.newLoanToValue}</p>
+                                </div>
                                 
-                            </div>
+                                <div className = {classes.modal_breakdown__section__div}>
+                                <p>Property Type :</p>
+                                <p>{props.breakDownValues.type}</p>
+                                </div>
+
+                                
+                            </section>
+
+                            <section className={classes.modal_breakdown__section}>
+                            <div className = {classes.modal_breakdown__section__div}>
+                                <p>Total Downpayment :</p>
+                                <p>{props.breakDownValues.saved}</p>
+                                </div>
+
+                                <div className = {classes.modal_breakdown__section__div}>
+                                <p>Minimum Downpayment :</p>
+                                <p>${props.breakDownValues.newDownPayment}</p>
+                                </div>
+
+                                
+
+                                <div className = {classes.modal_breakdown__section__div}>
+                                <p>Remaining Downpayment :</p>
+                                <p>${props.breakDownValues.newRemainderDownpayment} </p>
+                                </div>
+                            </section>
                         </div>
                     </Card>
                 </div>,
